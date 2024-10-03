36e5f099761afa2a1ad8640e1f4bdcfad75e07cb
CAFFENNE Hugo <158146784+Souyakoff@users.noreply.github.com>
2024-10-03 10:52:54 +0200

    Add files via upload

---
+++ randomuser/index.js
@@ -0,0 +1,36 @@
+console.log("Salut");
+
+const endpoint = "https://randomuser.me/api/?results=50";
+
+// Fetch
+fetch(endpoint)
+    .then(response => {
+        console.log(response.status);
+
+        if (response.status === 200) {
+            return response.json();
+        }
+        throw new Error('Erreur réseau');
+    })
+    .then(datas => {
+        console.log(datas);
+
+
+        const tableBody = document.getElementById("usersTable");
+        datas.results.forEach(user => {  // Corrigé ici
+            tableBody.innerHTML += `
+                <tr>
+                    <td>${user.login.username}</td>
+                    <td>${user.gender}</td>
+                    <td>${user.name.last}</td>
+                    <td>${user.name.first}</td>
+                    <td>${user.email}</td>
+                    <td>${user.location.country}</td>
+                    <td><img src="${user.picture.thumbnail}" alt="Photo de ${user.name.first}" /></td>
+                </tr>
+            `;
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
